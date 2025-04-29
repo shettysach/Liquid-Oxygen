39635b867c6a97f9e9289a591f30dd4ffe9bf3ab
shettysach <shettysachith47@gmail.com>
2025-04-29 05:43:50 +0530

    repl > multiline, but doesn't print intermed results

--- README.md
+++ README.md
@@ -2,16 +2,28 @@ Lox interpreter.
 Learning Haskell and Crafting Interpreters.
 
 ### Cabal
+
+#### REPL
+
+```sh
+cabal run liquid-oxygen
+```
+
+#### Interpret script
+
 ```sh
 cabal run liquid-oxygen <script>
 ```
 
 Resources
-- https://craftinginterpreters.com/
+- https://craftinginterpreters.com/ 
+
+> TODO:
+> - Standard function 
+> - Expression based REPL
 
 > **NOTES:**
 > - In my version, the resolver also takes into account global variables by seeding the scope stack with `[Map.empty]`.
 > - Only `modifyIORef` (lazy) works for `initialize` due to Functions (example: `closure <- lift (initialize name fun env)`), 
 because closures depend on lazily init envs and may involve cyclic refs (`mdo` / `fixIO`).
 > - `atomicModifyIORef` and `modifyIORef'` (strict) seems to work safely for `assignAt`, since assignment happens after eval and does not involve cyclic structs.
-> - Need to further experiment if `modifyIORef'` ever causes failures for `assignAt` in more complex scenarios.
--- src/Repl.hs
+++ src/Repl.hs
@@ -3,32 +3,36 @@
 
 module Repl where
 
-import Control.Monad ((>=>))
-import Data.Map      qualified as Map
-import System.IO     (hFlush, hPrint, stderr, stdout)
+import Data.Map    qualified as Map
+import System.IO   (hFlush, hPrint, stderr, stdout)
 
-import Environment   (Scope, global)
-import Interpreter   (interpretRepl)
-import Parser        (parse)
-import Resolver      (resolveRepl)
-import Scanner       (scan)
-import Syntax        (Env)
+import Environment (Scope, global)
+import Error       (ScanError)
+import Interpreter (interpretRepl)
+import Parser      (parse)
+import Resolver    (resolveRepl)
+import Scanner     (scan)
+import Syntax      (Env)
+import Token       as T
 
 repl :: IO ()
-repl = global >>= repl' [Map.empty]
+repl = do
+  putStrLn "Welcome to the REPL. Type `quit` to quit."
+  global >>= repl' [Map.empty]
 
 repl' :: [Scope] -> Env -> IO ()
 repl' scopes env = do
   putStr "> "
   hFlush stdout
-  getLine >>= \case
-    [] -> repl' scopes env
-    "quit" -> putStrLn "\nExiting REPL."
-    line -> runRepl line scopes env >>= uncurry repl'
+  readMultiline >>= \case
+    Right "quit" -> putStrLn "\nExiting REPL."
+    Right [] -> repl' scopes env
+    Right line -> runRepl line scopes env >>= uncurry repl'
+    Left err -> hPrint stderr err >> repl' scopes env
 
 runRepl :: String -> [Scope] -> Env -> IO ([Scope], Env)
 runRepl input scopes env =
-  (chainIO scan . Just >=> chainIO parse) input >>= \case
+  chainIO scan (Just input) >>= chainIO parse >>= \case
     Nothing -> pure (scopes, env)
     Just stmts -> case resolveRepl stmts scopes of
       Left err -> hPrint stderr err >> pure (scopes, env)
@@ -36,6 +40,8 @@ runRepl input scopes env =
         interpretRepl (stmts', dists') env >>= \case
           Left err -> hPrint stderr err >> pure (scopes, env)
           Right env' -> pure (scopes', env')
+
+--
 
 chainIO :: (Show l) => (a -> Either l r) -> Maybe a -> IO (Maybe r)
 chainIO _ Nothing = pure Nothing
@@ -50,3 +56,33 @@ endIO f (Just x) =
   f x >>= \case
     Left l -> hPrint stderr l
     Right _ -> pure ()
+
+--
+
+readMultiline :: IO (Either ScanError String)
+readMultiline = do
+  hFlush stdout
+  line <- getLine
+  case scan line of
+    Left err -> pure $ Left err
+    Right tokens ->
+      if braceCount tokens > 0
+        then readBlock [line] (braceCount tokens)
+        else pure $ Right line
+
+readBlock :: [String] -> Int -> IO (Either ScanError String)
+readBlock acc 0 = pure $ Right $ unlines $ reverse acc
+readBlock acc n = do
+  putStr "| "
+  hFlush stdout
+  next <- getLine
+  case scan next of
+    Right ts -> readBlock (next : acc) (n + braceCount ts)
+    Left err -> pure $ Left err
+
+braceCount :: [Token] -> Int
+braceCount = foldr count 0
+ where
+  count (T.LeftBrace, _) acc  = acc + 1
+  count (T.RightBrace, _) acc = acc - 1
+  count _ acc                 = acc
